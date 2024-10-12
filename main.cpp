abd3054915c4ba508bd14f6718adedbe21ae677f
MegaDarken <joe.swales@googlemail.com>
2024-10-12 23:26:33 +0100

    main args

--- main.cpp
+++ main.cpp
@@ -9,7 +9,13 @@
 
 enum argsMode {noArg, widthArg, heightArg, wordCountArg};
 
-constexpr size_t WIDTH_ARG_SHORT = stringHash("-w");
+const size_t WIDTH_ARG_SHORT = stringHash("-w");
+const size_t WIDTH_ARG = stringHash("--width");
+const size_t HEIGHT_ARG_SHORT = stringHash("-h");
+const size_t HEIGHT_ARG = stringHash("--height");
+const size_t WORDC_ARG_SHORT = stringHash("-c");
+const size_t WORDC_ARG = stringHash("--count");
+
 
 int main(int argc, char** argv)
 {
@@ -23,16 +29,47 @@ int main(int argc, char** argv)
 
     for (size_t i = 0; i < argc; i++)
     {
-        
-        switch (stringHash_nullTerminated(argv[i]))
+        switch (mode)
         {
-        case WIDTH_ARG_SHORT:
-            /* code */
+        case noArg:
+            switch (stringHash_nullTerminated(argv[i]))
+            {
+            case WIDTH_ARG_SHORT:
+            case WIDTH_ARG:
+                mode = widthArg;
+                break;
+
+            case HEIGHT_ARG_SHORT:
+            case HEIGHT_ARG:
+                mode = heightArg;
+                break;
+
+            case WORDC_ARG_SHORT:
+            case WORDC_ARG:
+                mode = wordCountArg;
+                break;
+            
+            default:
+                break;
+            }
+            break;
+        
+        case widthArg:
+            if (1 == sscanf(argv[i], "%d", &width)) mode = noArg;
+            break;
+
+        case heightArg:
+            if (1 == sscanf(argv[i], "%d", &height)) mode = noArg;
+            break;
+
+        case wordCountArg:
+            if (1 == sscanf(argv[i], "%zu", &wordCount)) mode = noArg;
             break;
         
         default:
             break;
         }
+        
     }
 
     //User inputs
