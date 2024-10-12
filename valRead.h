1cf8070e0de7bf1c90ea5fcef750daa0252fd6e6
MegaDarken <joe.swales@googlemail.com>
2024-10-12 21:52:44 +0100

    valRead header extern "C"

--- valRead.h
+++ valRead.h
@@ -3,13 +3,34 @@
 
 #include "stdlib.h"
 
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 void valRead_charDest(char *dest, char *invalidInputText);
+
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 char valRead_char(char *invalidInputText);
 
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 void valRead_intDest(int *dest, char *invalidInputText);
+
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 int valRead_int(char *invalidInputText);
 
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 void valRead_size_tDest(size_t *dest, char *invalidInputText);
+
+#ifdef __cplusplus
+extern "C" constexpr
+#endif //__cplusplus
 size_t valRead_size_t(char *invalidInputText);
 
 #endif //__VAL_READ__
