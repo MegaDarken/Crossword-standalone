d1ea98556e4e78b2ca67a6e93d414f42fbb9bb0c
MegaDarken <joe.swales@googlemail.com>
2024-10-13 22:36:05 +0100

    swapping dereference addresses for sizeof

--- swapping.h
+++ swapping.h
@@ -7,10 +7,10 @@
 
 #define swap_known(x,y) \
     do { \
-        unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1]; \
-        memcpy(swap_temp,y,sizeof(x)); \
-        memcpy(y,x,       sizeof(x)); \
-        memcpy(x,swap_temp,sizeof(x)); \
+        unsigned char swap_temp[sizeof(*x) == sizeof(*y) ? (signed)sizeof(*x) : -1]; \
+        memcpy(swap_temp,y,sizeof(*x)); \
+        memcpy(y,x,       sizeof(*x)); \
+        memcpy(x,swap_temp,sizeof(*x)); \
     } while(0)
 
 #define swap_size(x,y,size) \
