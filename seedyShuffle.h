cf5c985b278825d5f19d967448676992dce5ec62
MegaDarken <joe.swales@googlemail.com>
2024-10-16 10:44:40 +0100

    seedyShuffle.h

---
+++ seedyShuffle.h
@@ -0,0 +1,29 @@
+#ifndef SEEDY_SHUFFLE_
+#define SEEDY_SHUFFLE_
+
+#include "swapping.h"
+#include "randomTable.h"
+
+#define seedyShuffle_helper(_1, _2, _3, _4, NAME, ...) NAME
+
+#define seedyShuffle_known(array, count, seed) \
+    do { \
+        __typeof__ (count) _count = (count);\
+        for ( int i = 0; i < _count; i++ ) { \
+        swap(&array[i], &array[(getSeedyRandomUInt(seed) % _count)]); \
+        } \
+    } while(0)
+
+#define seedyShuffle_size(array, count, seed, elementSize) \
+    do { \
+        __typeof__ (count) _count = (count);\
+        size_t index = array;\
+        for ( size_t i = 0; i < _count; i++ ) { \
+        swap(index, array + ((getSeedyRandomUInt(seed) % _count) * elementSize), elementSize); \
+        index += elementSize;\
+        } \
+    } while(0)
+
+#define seedyShuffle(...) shuffle_helper(__VA_ARGS__, seedyShuffle_size, seedyShuffle_known)(__VA_ARGS__)
+
+#endif //SEEDY_SHUFFLE_
