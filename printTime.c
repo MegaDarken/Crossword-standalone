47db0b6a505778c1f7050452b8b82ff820418fa6
Joseph Swales <joe.swales@googlemail.com>
2024-10-29 09:53:36 +0000

    printTime.c mdy

--- printTime.c
+++ printTime.c
@@ -27,13 +27,31 @@ void fprintTime_dmy(FILE *stream)
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    fprintf(stream, "%02d-%02d-%d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
 #ifdef __cplusplus
 extern "C"
 #endif //__cplusplus
 void printTime_dmy()
+{
+    fprintTime_dmy(stdout);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
+void fprintTime_mdy(FILE *stream)
+{
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d\n", tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
+void printTime_mdy()
 {
     fprintTime_dmy(stdout);
 }
