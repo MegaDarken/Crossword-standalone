35f3f9f93242c08f8050af8430b640671620b74e
Joseph Swales <joe.swales@googlemail.com>
2024-10-29 09:46:03 +0000

    printTime header ymd and dmy

--- printTime.h
+++ printTime.h
@@ -6,11 +6,21 @@
 #ifdef __cplusplus
 extern "C"
 #endif //__cplusplus
-void fprintTime_Bigendian(FILE *stream);
+void fprintTime_ymd(FILE *stream);
 
 #ifdef __cplusplus
 extern "C"
 #endif //__cplusplus
-void printTime_Bigendian();
+void printTime_ymd();
+
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
+void fprintTime_dmy(FILE *stream);
+
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
+void printTime_dmy();
 
 #endif // __PRINT_TIME__
