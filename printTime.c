80bd112e39f25153c3156b7f421c4454a2f683bc
MegaDarken <joe.swales@googlemail.com>
2024-10-30 21:21:20 +0000

    printTime no newline

--- printTime.c
+++ printTime.c
@@ -9,7 +9,7 @@ void fprintTime_ymd(FILE *stream)
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    fprintf(stream, "%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fprintf(stream, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
 #ifdef __cplusplus
@@ -27,7 +27,7 @@ void fprintTime_dmy(FILE *stream)
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
 #ifdef __cplusplus
@@ -45,7 +45,7 @@ void fprintTime_mdy(FILE *stream)
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d\n", tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    fprintf(stream, "%02d/%02d/%d %02d:%02d:%02d", tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
 
 #ifdef __cplusplus
