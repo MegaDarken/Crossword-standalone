e70df600575bfb083dcecd74de03a9c66b627105
MegaDarken <joe.swales@googlemail.com>
2024-11-02 22:28:17 +0000

    clearInput extern "C"

--- clearInput.c
+++ clearInput.c
@@ -2,11 +2,17 @@
 
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
 void clearInput_until(const int val)
 {
     for (int ch = getchar(); ch != EOF && ch != '\n';ch = getchar());
 }
 
+#ifdef __cplusplus
+extern "C"
+#endif //__cplusplus
 void clearInput_untilNewLine()
 {
     clearInput_until('\n');
