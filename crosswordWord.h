4b67ae26e7efa7a4347bf7bdfc94b5f93e15987e
MegaDarken <joe.swales@googlemail.com>
2025-01-12 09:47:28 +0000

    crosswordWord

---
+++ crosswordWord.h
@@ -0,0 +1,18 @@
+#ifndef _CROSSWORD_WORD_
+#define _CROSSWORD_WORD_
+
+#include <stddef.h>
+
+#include "charArrayPair.h"
+
+enum crosswordFlag{none, across, down, acrossAndDown};
+
+struct crosswordPlacedWord
+{
+    struct charArrayPair pair;
+    size_t originalPairIndex;
+    enum crosswordFlag flag;
+    size_t gridIndex;   
+};
+
+#endif //_CROSSWORD_WORD_
